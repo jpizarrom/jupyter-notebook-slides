6604df88f37537248ac9814eae4033b6901c5a68
Juan Pizarro <jpizarrom@gmail.com>
2017-06-14 01:52:05 -0400

    Update

--- publish.sh
+++ publish.sh
@@ -10,6 +10,6 @@ git checkout -B gh-pages
 cp -r /tmp/workspace/* .
 git add -A .
 git commit -m "new version"
-git push origin master gh-pages
+git push origin master gh-pages -f
 git checkout master
 rm -rf /tmp/workspace
